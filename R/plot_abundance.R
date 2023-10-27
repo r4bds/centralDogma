c17d58a945024e8b208008e63a7e5f66d2bbfb3a
Søren Helweg Dam <soren.helweg@gmail.com>
2023-10-27 12:23:13 +0200

    Make compatible with R4.0

--- R/plot_abundance.R
+++ R/plot_abundance.R
@@ -1,23 +1,24 @@
 #' Plot abundance of each amino acid in a peptide
 #'
 #' @param peptide A peptide
+#' @importFrom magrittr `%>%`
 #'
 #' @return A bar chart of amino acid content
 #' @export
 #'
 plot_abundance <- function(peptide){
-  unique_amino_acids <- peptide |>
-    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
-    as.character() |>
+  unique_amino_acids <- peptide %>%
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) %>%
+    as.character() %>%
     unique()
 
-  counts <- sapply(unique_amino_acids, function(amino_acid) stringr::str_count(string = peptide, pattern =  amino_acid)) |>
+  counts <- sapply(unique_amino_acids, function(amino_acid) stringr::str_count(string = peptide, pattern =  amino_acid)) %>%
     as.data.frame()
 
   colnames(counts) <- c("Counts")
   counts[["Peptide"]] <- rownames(counts)
 
-  plt_abundance <- counts |>
+  plt_abundance <- counts %>%
     ggplot2::ggplot(ggplot2::aes(x = .data$Peptide, y = .data$Counts, fill = .data$Peptide)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw()
